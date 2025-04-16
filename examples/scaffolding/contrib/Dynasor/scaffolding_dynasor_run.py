807d8a71acb190496df695a0c05e14bbe8b55284
Fsanic <zheyufu2@gmail.com>
2025-04-16 08:46:10 +0000

    Initial commit.
    Signed-off-by: Fsanic <zheyufu2@gmail.com>

---
+++ examples/scaffolding/contrib/Dynasor/scaffolding_dynasor_run.py
@@ -0,0 +1,52 @@
+import argparse
+
+from dynasor_controller import DynasorGenerationController
+
+from tensorrt_llm.scaffolding import ScaffoldingLlm, TRTLLMWorker
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--generation_dir",
+                        type=str,
+                        default="./models/DeepSeek-R1-Distill-Qwen-7B")
+    args = parser.parse_args()
+    return args
+
+
+def test_sync(prompts, proposer_worker, generation_dir):
+    prototype_controller = DynasorGenerationController(generation_dir,
+                                                       max_tokens=7000)
+    llm = ScaffoldingLlm(
+        prototype_controller,
+        {DynasorGenerationController.WorkerTag.GENERATION: proposer_worker},
+    )
+    results = llm.generate(prompts)
+    for result in results:
+        print(result.output.output_str)
+    print(f"main shutting down...")
+    llm.shutdown()
+    print(f"worker shutting down...")
+    proposer_worker.shutdown()
+    print(f"main shut down done")
+
+
+def main():
+    args = parse_arguments()
+
+    prompts = [
+        "Natalia sold clips to 48 of her friends in April, and then she sold half as many clips in May. How many clips did Natalia sell altogether in April and May?\r\n\r\n",
+        "There exist real numbers $x$ and $y$, both greater than 1, such that $\\log_x\\left(y^x\\right)=\\log_y\\left(x^{4y}\\right)=10$. Find $xy$.",
+        "Find the largest possible real part of \\[(75+117i)z+\\frac{96+144i}{z}\\]where $z$ is a complex number with $|z|=4$.",
+    ]
+
+    # the max_num_tokens here should larger or equal to max_tokens of DynasorGenerationController
+    llm_worker = TRTLLMWorker.init_with_new_llm(args.generation_dir,
+                                                backend="pytorch",
+                                                max_num_tokens=7000)
+
+    test_sync(prompts, llm_worker, args.generation_dir)
+
+
+if __name__ == "__main__":
+    main()
